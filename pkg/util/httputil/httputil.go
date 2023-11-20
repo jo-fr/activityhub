32d7fd219e0bc0c2a0df2c94a3b63f47a1f8719a
jo-fr <jonathan@freibergers.de>
2023-11-20 15:57:49 +0100

    mv httputil to pkg

---
+++ pkg/util/httputil/httputil.go
@@ -0,0 +1,49 @@
+package httputil
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+// UnmarshalRequestBody unmarshals the request body into the given type. The request body is then reset to its original state.
+func UnmarshalRequestBody[T any](r *http.Request) (T, error) {
+	var v T
+	body, err := GetBody(r)
+	if err != nil {
+		return v, errors.Wrap(err, "failed to read request body")
+	}
+
+	err = json.Unmarshal(body.Bytes(), &v)
+	return v, errors.Wrap(err, "failed to unmarshal request body")
+}
+
+func UnmarshalResponseBody[T any](r *http.Response) (T, error) {
+	var v T
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return v, errors.Wrap(err, "failed to read request body")
+	}
+
+	r.Body = io.NopCloser(bytes.NewReader(body))
+
+	err = json.Unmarshal(body, &v)
+	return v, errors.Wrap(err, "failed to unmarshal request body")
+}
+
+// GetBody reads the request body and returns it as a bytes.Buffer. The request body is then reset to its original state.
+func GetBody(r *http.Request) (*bytes.Buffer, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := bytes.NewBuffer(body)
+
+	r.Body = io.NopCloser(buffer)
+
+	return buffer, nil
+}
