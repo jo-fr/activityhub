9523dfda237fb050da82fcee7b480e3400c55959
jo-fr <jonathan@freibergers.de>
2023-11-20 16:31:50 +0100

    refactor inbox response

--- modules/activitypub/inbox.go
+++ modules/activitypub/inbox.go
@@ -1,31 +1,115 @@
 package activitypub
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"strings"
 
+	"github.com/jo-fr/activityhub/modules/activitypub/internal/keys/httprequest"
 	"github.com/jo-fr/activityhub/modules/activitypub/models"
+	model "github.com/jo-fr/activityhub/modules/api/externalmodel"
+	"github.com/jo-fr/activityhub/pkg/util/httputil"
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
 )
 
-func (h *Handler) ReceiveInboxActivity(ctx context.Context, actor string, object string, activityType string) (models.Follower, error) {
+func (h *Handler) ReceiveInboxActivity(ctx context.Context, activity model.Activity) error {
 
-	accountName := getAccountFromURI(object)
+	obj := activity.Object.(string)
+
+	accountName := getAccountFromURI(obj)
 	account, err := h.store.GetAccoutByUsername(ctx, accountName)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return models.Follower{}, ErrActorNotFound
+			return ErrActorNotFound
 		}
-		return models.Follower{}, errors.Wrap(err, "failed to get actor from db")
+		return errors.Wrap(err, "failed to get actor from db")
 	}
 
-	follow, err := h.store.CreateFollow(ctx, account.ID, object)
+	_, err = h.store.CreateFollow(ctx, account.ID, activity.Actor)
 	if err != nil {
-		return models.Follower{}, errors.Wrap(err, "failed to create follow")
+		return errors.Wrap(err, "failed to create follow")
 	}
 
-	return follow, nil
+	if err := returnAcceptActivity(ctx, account, activity); err != nil {
+		return errors.Wrap(err, "failed to return accept activity")
+	}
+
+	return nil
+}
+
+func returnAcceptActivity(ctx context.Context, account models.Account, activity model.Activity) error {
+
+	obj := activity.Object.(string)
+
+	ma := model.Activity{
+		Context: "https://www.w3.org/ns/activitystreams",
+		ID:      activity.ID,
+		Type:    "Accept",
+		Actor:   obj,
+		Object:  activity,
+	}
+
+	inboxURL, err := GetInboxURL(activity.Actor)
+	if err != nil {
+		return errors.Wrap(err, "failed to get inbox url")
+	}
+
+	json, err := json.Marshal(ma)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal activity")
+	}
+
+	req, err := httprequest.New(http.MethodPost, inboxURL, bytes.NewBuffer(json))
+	if err != nil {
+		return errors.Wrap(err, "failed to create request")
+	}
+
+	if err := req.Sign(account.PrivateKey, obj); err != nil {
+		return errors.Wrap(err, "failed to sign request")
+	}
+
+	resp, err := req.Do()
+	if err != nil {
+		return errors.Wrap(err, "failed to send request")
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errBody, err := httputil.UnmarshalResponseBody[map[string]any](resp)
+		if err != nil {
+			return errors.Wrap(err, "failed to unmarshal response body")
+		}
+		return fmt.Errorf("received status code %d. Response: %s", resp.StatusCode, errBody)
+	}
+
+	return nil
+}
+
+func GetInboxURL(actorURI string) (string, error) {
+	req, err := httprequest.New(http.MethodGet, actorURI, nil)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create request")
+	}
+
+	resp, err := req.Do()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get actor")
+	}
+
+	responseMap, err := httputil.UnmarshalResponseBody[map[string]any](resp)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to unmarshal response body")
+	}
+
+	inbox, ok := responseMap["inbox"].(string)
+	if !ok {
+		return "", errors.New("failed to get inbox from actor")
+	}
+
+	return inbox, nil
 }
 
 // getAccountFromURI returns the account name from an URI
