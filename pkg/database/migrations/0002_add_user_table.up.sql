1488bbf8b94dcf489bcebda9362a8e7cd0ae3413
jo-fr <jonathan@freibergers.de>
2023-11-20 14:41:35 +0100

    improve migrations

--- pkg/database/migrations/0002_add_user_table.up.sql
+++ pkg/database/migrations/0002_add_user_table.up.sql
@@ -1,18 +1,20 @@
 BEGIN;
     CREATE TABLE IF NOT EXISTS activityhub.account (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        deleted_at TIMESTAMP,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        deleted_at TIMESTAMPTZ,
         preferred_username VARCHAR(255) NOT NULL,
         name VARCHAR(255) NOT NULL,
         summary VARCHAR(255) NOT NULL,
         private_key BYTEA NOT NULL,
-        public_key BYTEA NOT NULL
+        public_key BYTEA NOT NULL,
+
+        CONSTRAINT unique_preferred_username UNIQUE (preferred_username,deleted_at),
+        CONSTRAINT unique_public_key UNIQUE (public_key,deleted_at),
+        CONSTRAINT unique_private_key UNIQUE (private_key,deleted_at)
     );
 
     CREATE INDEX IF NOT EXISTS account_deleted_at_idx ON activityhub.account (deleted_at);
-    CREATE UNIQUE INDEX IF NOT EXISTS account_preferred_username_idx ON activityhub.account (preferred_username,deleted_at);
-    CREATE UNIQUE INDEX IF NOT EXISTS account_public_key_idx ON activityhub.account (public_key,deleted_at);
-    CREATE UNIQUE INDEX IF NOT EXISTS account_private_key_idx ON activityhub.account (private_key,deleted_at);
+
 COMMIT;
--- pkg/database/migrations/0003_add_follower_table.up.sql
+++ pkg/database/migrations/0003_add_follower_table.up.sql
@@ -1,15 +1,16 @@
 BEGIN;
     CREATE TABLE IF NOT EXISTS activityhub.follower (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        deleted_at TIMESTAMP,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        deleted_at TIMESTAMPTZ,
 
         account_id_followed UUID REFERENCES activityhub.account(id) NOT NULL,
-        account_uri_following VARCHAR(255) NOT NULL
+        account_uri_following VARCHAR(255) NOT NULL,
+
+        CONSTRAINT unique_follower_account_id_followed_account_uri_following UNIQUE (deleted_at, account_id_followed, account_uri_following)
 
     );
 
     CREATE INDEX IF NOT EXISTS follower_deleted_at_idx ON activityhub.follower (deleted_at);
-
 COMMIT;
