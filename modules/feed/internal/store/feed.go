3678efc9f48429b5dc86a9a1bc367a5d0817fc22
jo-fr <jonathan@freibergers.de>
2023-11-23 17:29:48 +0100

    add GetSourceFeedWithID

--- modules/feed/internal/store/feed.go
+++ modules/feed/internal/store/feed.go
@@ -15,7 +15,15 @@ func (s *Store) CreateSourceFeed(ctx context.Context, source model.SourceFeed) (
 
 func (s *Store) GetSourceFeedWithFeedURL(ctx context.Context, feedURL string) (model.SourceFeed, error) {
 	var source model.SourceFeed
-	if err := s.db.WithContext(ctx).Where("url = ?", feedURL).First(&source).Error; err != nil {
+	if err := s.db.WithContext(ctx).First(&source, "url = ?", feedURL).Error; err != nil {
+		return model.SourceFeed{}, err
+	}
+	return source, nil
+}
+
+func (s *Store) GetSourceFeedWithID(ctx context.Context, id string) (model.SourceFeed, error) {
+	var source model.SourceFeed
+	if err := s.db.WithContext(ctx).First(&source, "id = ?", id).Error; err != nil {
 		return model.SourceFeed{}, err
 	}
 	return source, nil
