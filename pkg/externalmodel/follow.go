13bfa1a402d1a7193f37dedbdd443730d1138a44
jo-fr <jonathan@freibergers.de>
2023-11-28 14:50:38 +0100

    Update Activity struct in externalmodel package

--- pkg/externalmodel/follow.go
+++ pkg/externalmodel/follow.go
@@ -1,9 +1,14 @@
 package externalmodel
 
 type Activity struct {
-	Context string `json:"@context"`
-	ID      string `json:"id"`
-	Type    string `json:"type"`
-	Actor   string `json:"actor"`
-	Object  any    `json:"object"`
+	Context      any      `json:"@context"`
+	ID           string   `json:"id"`
+	Type         string   `json:"type"`
+	Actor        string   `json:"actor"`
+	Object       any      `json:"object"`
+	Published    string   `json:"published,omitempty"`
+	AttributedTo string   `json:"attributedTo,omitempty"`
+	Content      string   `json:"content,omitempty"`
+	To           []string `json:"to,omitempty"`
+	Sensitive    bool     `json:"sensitive,omitempty"`
 }
