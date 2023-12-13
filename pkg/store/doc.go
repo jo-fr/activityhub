19ddd650f0b1f542eda42134916e2eafd76d38d0
jo-fr <jonathan@freibergers.de>
2023-12-13 15:13:47 +0100

    add comment

---
+++ pkg/store/doc.go
@@ -0,0 +1,5 @@
+// Package store provides a generic data store, allowing for the execution of repository
+// functions within a database transaction. It is designed to work with types implementing
+// the IRepository interface.
+
+package store
