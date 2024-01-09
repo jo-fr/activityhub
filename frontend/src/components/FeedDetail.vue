732d233031092573a94c07d1b9ec7c8089bc5efd
jo-fr <jonathan@freibergers.de>
2024-01-09 15:29:14 +0100

    add status

--- frontend/src/components/FeedDetail.vue
+++ frontend/src/components/FeedDetail.vue
@@ -1,26 +1,50 @@
 <template>
     <div>
-      <h1>{{data.name}}</h1>
-      <p>{{data.description}}</p>
+      <h1>{{data?.feed?.name}}</h1>
+      <div clas="links">
+        <div>
+          {{ data?.feed?.account.preferredUsername }}
+        </div>
+        <div>
+         <a target=”_blank” :href="data?.feed?.feedURL">Feed URL</a>
+        </div>
+        <div>
+         <a target=”_blank” :href="data?.feed?.hostURL">Host Website</a>
+        </div>
+      </div>
+      <p>{{data?.feed?.description}}</p>
+
+        <div>
+          <h2>Feed</h2>
+            <div class="item" v-for="status in data?.status" :key="status.createdAt" v-html="status.Content"></div>
+        </div>
     
     </div>
   </template>
   
   <script lang="ts">
   import { defineComponent, ref, onMounted, defineProps  } from 'vue';
-  import { useRoute } from 'vue-router'; 
+  import { stringifyQuery, useRoute } from 'vue-router'; 
   import type { Ref } from 'vue';
   
   interface Feed {
-  name: string;
-  type: string;
-  feedURL: string;
-  hostURL: string;
-  author: string;
-  description: string;
-  imageURL: string;
-  accountID: string;
-}
+    name: string;
+    type: string;
+    feedURL: string;
+    hostURL: string;
+    author: string;
+    description: string;
+    imageURL: string;
+    accountID: string;
+    account: {
+      preferredUsername: string;
+  };
+  }
+
+  interface Status {
+    createdAt: string;
+    Content: string
+  }
 
   
   export default defineComponent({
@@ -28,7 +52,10 @@
 
 
     setup() {
-        const data: Ref<Feed> = ref({name: "", type: "", feedURL: "", hostURL: "", author: "", description: "", imageURL: "", accountID: ""});
+      const data: Ref<{ feed: Feed | null; status: Status[] | null }> = ref({
+      feed: null,
+      status: null,
+    });
         const route = useRoute();
 
 
@@ -43,7 +70,22 @@
                 // Parse the JSON response
                 const jsonData = await response.json();
                 // Update the data property with the fetched data
-                data.value = jsonData;
+                data.value.feed = jsonData;
+            }
+            catch (error) {
+                console.error('Error fetching data:', error);
+            }
+            try {
+                // Make a fetch request using TypeScript
+                const response = await fetch(`/api/feed/${route.params.id}/status?limit=10`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                // Parse the JSON response
+                const jsonData = await response.json();
+                // Update the data property with the fetched data
+                console.log(jsonData)
+                data.value.status = jsonData.items;
             }
             catch (error) {
                 console.error('Error fetching data:', error);
@@ -56,6 +98,27 @@
 
   
   <style scoped>
-  /* Add your component styles here */
+  .item {
+    border: 1px solid var( --color-border);
+    border-radius: 4px;
+    padding: 10px;
+    margin: 10px 0;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+  }
+
+  .links > div {
+    display: inline;;
+  }
+
+  strong {
+    color: forestgreen;
+    font-weight: 800;
+  }
   </style>
   
